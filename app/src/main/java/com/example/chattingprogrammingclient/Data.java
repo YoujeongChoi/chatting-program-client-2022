2d32464c5ffe3d0bdd1fa8ab86eae937c7dfb8e9
YoujeongChoi <>
2022-07-10 18:49:32 +0900

    Changes

---
+++ app/src/main/java/com/example/chattingprogrammingclient/Data.java
@@ -0,0 +1,41 @@
+
+package com.example.chattingprogrammingclient;
+public class Data {
+
+    String userId;
+    String msg;
+    String time;
+
+    // getter and setter
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    // Constructor
+    public Data(String userId, String msg, String time) {
+        this.userId = userId;
+        this.msg = msg;
+        this.time = time;
+    }
+
+}
---
+++ app/src/main/java/com/example/chattingprogrammingclient/MessageAdapter.java
@@ -0,0 +1,58 @@
+package com.example.chattingprogrammingclient;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    private ArrayList<Data> list;
+
+    public MessageAdapter(ArrayList<Data> list) {
+        this.list = list;
+    }
+
+
+    @NonNull
+    @Override
+    public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.message_list_item, parent, false);
+        return new MessageAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.ViewHolder holder, int position) {
+        holder.userID.setText(list.get(position).userId);
+        holder.chatMsg.setText(list.get(position).msg);
+        holder.time.setText(list.get(position).time);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != list ? list.size() :0);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView userID;
+        TextView chatMsg;
+        TextView time;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userID = itemView.findViewById(R.id.userId);
+            chatMsg = itemView.findViewById(R.id.chatMessage);
+            time = itemView.findViewById(R.id.time);
+        }
+    }
+
+}
